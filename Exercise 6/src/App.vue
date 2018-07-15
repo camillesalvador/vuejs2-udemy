fb199c765f783c52c8ec1fdb38fa889fe79bd54c
jemHerrera <camille.salvador@outlook.com>
2018-07-14 21:50:48 -0600

    solution for exercise 6

---
+++ Exercise 6/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="container">
+        <server-header></server-header>
+        <hr>
+        <div class="row">
+            <div class="col-xs-12 col-sm-6">
+                <ul class="list-group">
+                    <li
+                            class="list-group-item"
+                            v-for="index in 5" :key="index">
+                        Server #{{ index }}
+                    </li>
+                </ul>
+            </div>
+            <server-detail></server-detail>
+        </div>
+        <hr>
+        <server-footer></server-footer>
+    </div>
+</template>
+
+<script>
+import ServerHeader from './components/ServerHeader.vue'
+import ServerFooter from './components/ServerFooter.vue'
+import ServerDetail from './components/ServerDetail.vue'
+
+export default {
+    components: {
+        ServerHeader,
+        ServerFooter,
+        ServerDetail
+    }
+}
+</script>
+
+<style>
+
+</style>
